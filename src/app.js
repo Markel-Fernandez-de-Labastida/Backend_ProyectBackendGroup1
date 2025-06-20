7fbea50abb905f8c638af38daf0188efb2ced32c
maiferna <maiderfernandez57@gmail.com>
2025-06-20 12:31:57 +0200

    ADD: render link

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const whiteList = ['http://localhost:3000', 'http://localhost:5000', 'https://frontend-movie-app-b8in.onrender.com'];
+const whiteList = ['http://localhost:3000', 'http://localhost:5000', 'https://frontend-movie-app-b8in.onrender.com', 'https://frontend-proyectbackendgroup1.onrender.com'];
 app.use(cors({
     origin: whiteList
 }));
